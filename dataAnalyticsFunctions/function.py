5f47bf8ce2bd1dd2f782fffb5b155a5cd72cbab3
AkinariHashimoto <akinari0206@gmail.com>
2025-03-18 10:49:35 +0900

    first

---
+++ dataAnalyticsFunctions/function.py
@@ -0,0 +1,41 @@
+def hello():
+    return "Hello from my_package!"
+
+'''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+#from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import KFold
+
+#nullチェック
+def nullCheck(df):
+  print("欠損値を含むカラム")
+  for c in df.columns:
+    if df[c].isnull().sum() != 0:
+      print(c + "　：　" + str(df[c].isnull().sum()))
+
+#カラムの型
+def columnsType(df):
+  print("カラムの型")
+  for c in df.columns:
+    print(c + "　：　" + str(df[c].dtypes))
+
+#数値のみカラムの相関
+def corr(df, x, y):
+  #数値のみカラム
+  numeric_columns = df.select_dtypes(include=['number']).columns
+
+  #相関行列を求める
+  corr_matrix = df[numeric_columns].corr()
+
+  #相関行列を図示
+  plt.figure(figsize=(x, y))
+  sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+  plt.title('Correlation Matrix')
+  plt.show()
+
+  '''
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dataAnalyticsFunctions",  # パッケージ名（フォルダ名と同じ）
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas"
+    ],
+    author="Akinari Hashimoto",
+    author_email="akinari020637@gmail.com",
+    description="using data analytics",
+    url="https://github.com/AkinariHashimoto/dataAnalyticsFunctions",  # GitHubリポジトリのURL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
